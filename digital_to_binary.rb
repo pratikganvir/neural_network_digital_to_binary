d389f0cec96e7df45d8a86e2dd11e994ab7fc856
Pratik Ganvir <pratik.ganvir@ril.com>
2018-01-28 15:17:51 +0530

    Fixed summation of inputs to weights

--- digital_to_binary.rb
+++ digital_to_binary.rb
@@ -16,35 +16,38 @@ test_data = [
 [0,0,0,0,0,0,0,0,0,1]
 ]
 
-outputs = [
-[0,0,0,0],
-[0,0,0,1],
-[0,0,1,0],
-[0,0,1,1],
-[0,1,0,0],
-[0,1,0,1],
-[0,1,1,0],
-[0,1,1,1],
-[1,0,0,0],
-[1,0,0,1]
-]
+# outputs = [
+# [0,0,0,0],
+# [0,0,0,1],
+# [0,0,1,0],
+# [0,0,1,1],
+# [0,1,0,0],
+# [0,1,0,1],
+# [0,1,1,0],
+# [0,1,1,1],
+# [1,0,0,0],
+# [1,0,0,1]
+# ]
 
+outputs = [0,0,0,0,0,0,0,0,1,1]
 
-weights = []#Array.new(10).map {|a| rand }
-10.times { weights << Array.new(4,rand) }
-weights = weights.transpose
+weights = Array.new(10).map {|a| rand }
+# 10.times { weights << Array.new(4,rand) }
+# weights = weights.transpose
 initial_weights = weights.dup
 
 biases = Array.new(10,rand)
+puts weights.join(', ')
 
-1.times do
-	test_data.each_with_index do |input,index|
-		l0 = sigmoid(summation(input,weights))
-		error = outputs[index].map.with_index {|o,mindex| o-l0[mindex] }
-		slope = derivative(input)
-		weights = weights.map {|l1| l1*error.sample }
-		#weights = summation_if_not_0(weights,slope)
-	end
+100.times do |outer_index|
+	#test_data.each_with_index do |input,index|
+		l0 = sigmoid(matrix_multiplication(test_data,weights))
+		error = outputs.map.with_index {|o,mindex| o-l0[mindex] }
+		slope = derivative(l0)
+		new_weights = product_one_dimensional(error,slope)
+		weights = add_arrays(weights,new_weights)
+		puts weights.join(', ')
+	#end
 end
 
 puts "Initial weights: #{initial_weights}" 
@@ -54,8 +57,8 @@ puts "Testing neural network:"
 
 test_data.each_with_index do |input,index|
 	puts "Input: #{input}"
-	puts "Expected Output:: #{outputs[index]}"
-	output = sigmoid(summation(input,weights)).map {|o| (o < 0.5) ? 0 : 1 }
+	puts "Expected Output:: #{outputs}"
+	output = sigmoid(matrix_multiplication(test_data,weights)).map {|o| (o < 0.5) ? 0 : 1 }
 	puts "Output given by neural network: #{output}"
-	puts output==outputs[index] ? "Pass" : "Fail"
+	puts output==outputs ? "Pass" : "Fail"
 end
--- summation.rb
+++ summation.rb
@@ -17,3 +17,19 @@ def summation_if_not_0(array1,array2)
   }
   product
 end
+
+def product_one_dimensional(array1,array2)
+  array2.zip(array1).map {|a1,a2| a1*a2 }
+end
+
+def sum_error_with_weights(weights,errors)
+  weights.map.with_index {|w,index| w.map {|w1| w1*errors[index] }.sum }
+end
+
+def add_arrays(weights,new_weights)
+  weights.zip(new_weights).map {|w1,w2| w1+w2 }
+end
+
+def matrix_multiplication(array1,array2)
+  array1.map {|a1| a1.zip(array2).map {|a,b| a*b }.sum }
+end
